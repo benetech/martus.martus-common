c9b873a8aaba0f006020b6ba619a4ef0a440455d
charles <charles@localhost>
2003-12-09 19:21:42 +0000

    changed InputStreamReader to a UnicodeReader

--- source/org/martus/common/MartusUtilities.java
+++ source/org/martus/common/MartusUtilities.java
@@ -33,7 +33,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.SecureRandom;
@@ -422,7 +421,7 @@ public class MartusUtilities
 			return false;
 
 		final String encryptedTag = MartusXml.getTagStart(MartusXml.EncryptedFlagElementName);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(fdpInputStream));
+		BufferedReader reader = new BufferedReader(new UnicodeReader(fdpInputStream));
 		String thisLine = null;
 		while( (thisLine = reader.readLine()) != null)
 		{
