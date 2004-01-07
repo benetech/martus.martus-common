225b8f811b2b591c1be0edbf8941a8576a939eac
chiaweil <chiaweil@localhost>
2004-01-07 19:43:53 +0000

    refactory

---
+++ source/org/martus/common/clientside/test/TestDateUtilities.java
@@ -0,0 +1,54 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2001-2003, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+
+package org.martus.common.clientside.test;
+
+import org.martus.common.clientside.ChoiceItem;
+import org.martus.common.clientside.DateUtilities;
+import org.martus.common.test.TestCaseEnhanced;
+
+public class TestDateUtilities extends TestCaseEnhanced
+{
+
+	public TestDateUtilities(String name)
+	{
+		super(name);
+	}
+
+	public void testDateFormats()
+	{
+		ChoiceItem[] items = DateUtilities.getDateFormats();
+		assertTrue("Should have multiple date formats", items.length > 1);
+	}
+
+	public void testMdyOrder()
+	{
+		assertEquals("dmy", DateUtilities.getMdyOrder("dd/mm/yy"));
+		assertEquals("mdy", DateUtilities.getMdyOrder("MM-dd-yyyy"));
+		assertEquals("ymd", DateUtilities.getMdyOrder("y.mmmmmmm.yy.d"));
+	}
+
+}
