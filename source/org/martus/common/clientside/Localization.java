0babf47814b2a3bb59ab7d1f114dfc6cd06f4dcc
chiaweil <chiaweil@localhost>
2004-05-05 18:30:29 +0000

    bug#1315 - add khmer and nepali to language list for bulletins (mtf)

--- source/org/martus/common/clientside/Localization.java
+++ source/org/martus/common/clientside/Localization.java
@@ -348,11 +348,11 @@ public class Localization
 	public static final String ENGLISH = "en";
 	public static final String SPANISH = "es";
 	public static final String RUSSIAN = "ru";
-	public static final String THAI = "th";
+	public static final String THAI = "th";	
 	public static final String[] AVAILABLE_MTF_LANGUAGE_RESOURCES = {SPANISH, RUSSIAN, THAI};
 	public static final String[] ALL_LANGUAGE_CODES = {
 				LANGUAGE_OTHER, ENGLISH, "ar",
-				"az", "bn", "my","zh", "nl", "eo", "fa", "fr", "de","gu","ha","he","hi","hu",
-				"it", "ja","jv","kn","kk","ky","ko","ml","mr","or","pa","ps","pl","pt","ro",RUSSIAN,"sr",
+				"az", "bn", "km","my","zh", "nl", "eo", "fa", "fr", "de","gu","ha","he","hi","hu",
+				"it", "ja","jv","kn","kk","ky","ko","ml","mr","ne","or","pa","ps","pl","pt","ro",RUSSIAN,"sr",
 				"sr", "sd","si",SPANISH,"ta","tg","te",THAI,"tr","tk","uk","ur","uz","vi"};
 }
