78bcbc3bc5e511f2a3ac8653763821ab089dd938
kevins <kevins@localhost>
2005-09-02 15:20:52 +0000

    Revert potentially dangerous/incorrect changes

--- source/org/martus/common/test/TestBulletin.java
+++ source/org/martus/common/test/TestBulletin.java
@@ -141,9 +141,9 @@ public class TestBulletin extends TestCaseEnhanced
     	b.set(Bulletin.TAGAUTHOR, sampleAuthor);
     	assertTrue("didn't find author?", b.contains(sampleAuthor));
     	
-    	final String samplePrivateText = "shhh! they might be listening!";
-    	b.set(Bulletin.TAGPRIVATEINFO, samplePrivateText);
-    	assertTrue("didn't find private?", b.contains(samplePrivateText));
+    	final String samplePrivate = "shhh! they might be listening!";
+    	b.set(Bulletin.TAGPRIVATEINFO, samplePrivate);
+    	assertTrue("didn't find private?", b.contains(samplePrivate));
 
 		b.addPublicAttachment(proxy1);
     	assertTrue("didn't find public attachment?", b.contains(proxy1.getLabel()));
@@ -340,6 +340,7 @@ public class TestBulletin extends TestCaseEnhanced
 			public int encryptWasCalled;
 		}
 
+		BulletinStore store = new BulletinStore();
 		File tempDir = createTempDirectory();
 		MyMockDatabase db = new MyMockDatabase();
 		store.doAfterSigninInitialization(tempDir, db);
