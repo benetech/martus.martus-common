6880c94d15cfe906424831a19146ce666a8761d4
kevins <kevins@localhost>
2006-02-22 14:20:00 +0000

    Add more speed tests

--- source/org/martus/common/bulletinstore/TestLeafNodeCache.java
+++ source/org/martus/common/bulletinstore/TestLeafNodeCache.java
@@ -25,11 +25,20 @@ Boston, MA 02111-1307, USA.
 */
 package org.martus.common.bulletinstore;
 
+import java.io.File;
+
+import org.martus.common.bulletin.Bulletin;
+import org.martus.common.crypto.MartusSecurity;
+import org.martus.common.crypto.MockMartusSecurity;
 import org.martus.common.database.DatabaseKey;
+import org.martus.common.database.ServerFileDatabase;
+import org.martus.common.packet.BulletinHeaderPacket;
 import org.martus.common.packet.BulletinHistory;
 import org.martus.common.packet.UniversalId;
 import org.martus.util.Stopwatch;
 import org.martus.util.TestCaseEnhanced;
+import org.martus.util.UnicodeStringWriter;
+import org.martus.util.inputstreamwithseek.StringInputStreamWithSeek;
 
 public class TestLeafNodeCache extends TestCaseEnhanced
 {
@@ -63,5 +72,64 @@ public class TestLeafNodeCache extends TestCaseEnhanced
 		}
 		long millisFor1000 = watch.elapsed();
 		assertTrue("Took too long?", millisFor1000 < millisFor500 * 4);
+	}
+	
+	public void testLeafNodeCacheDiskSpeed() throws Exception
+	{
+		BulletinStore store = new BulletinStore();
+		File tempDirectory = createTempDirectory();
+		MartusSecurity security = MockMartusSecurity.createServer();
+		ServerFileDatabase db = new ServerFileDatabase(tempDirectory, security);
+		store.doAfterSigninInitialization(tempDirectory, db);
+		
+		MartusSecurity clientSecurity = MockMartusSecurity.createClient();
+		Stopwatch watch = new Stopwatch();
+		for(int i = 0; i < 100; ++i)
+		{
+			Bulletin testBulletin = new Bulletin(clientSecurity);
+			store.saveBulletinForTesting(testBulletin);
+		}
+//		System.out.println("Time to create bulletins: " + watch.elapsed());
+
+		watch.start();
+		LeafNodeCache cache = new LeafNodeCache(store);
+		assertFalse("cache not flushed?", cache.isCacheValid());
+		cache.getLeafKeys();
+		long buildCacheTime = watch.elapsed();
+//		System.out.println("Time for initial cache build: " + buildCacheTime);
+		assertTrue("Leaf key cache  build too slow? (was " + buildCacheTime + ")", buildCacheTime < 1000);
+		
+		watch.start();
+		cache.getLeafKeys();
+		long queryCacheTime = watch.elapsed();
+//		System.out.println("Time to query using the cache: " + queryCacheTime);
+		assertTrue("Time to query using cache too slow? (was " + queryCacheTime + ")", queryCacheTime < 100);
+		
+		Bulletin b = new Bulletin(clientSecurity);
+		UnicodeStringWriter writer = UnicodeStringWriter.create();
+		watch.start();
+		b.getBulletinHeaderPacket().writeXml(writer, clientSecurity);
+//		long timeToWrite = watch.elapsed();
+//		System.out.println(timeToWrite);
+		String xml = writer.toString();
+		
+		BulletinHeaderPacket bhp = new BulletinHeaderPacket();
+		watch.start();
+		for(int i = 0; i < 1000; ++i)
+		{
+			StringInputStreamWithSeek in = new StringInputStreamWithSeek(xml);
+			try
+			{
+				bhp.loadFromXml(in, null, clientSecurity);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+				throw(e);
+			}
+		}
+		long readHeadersTime = watch.elapsed();
+//		System.out.println("Time to read header packets: " + readHeadersTime);
+		assertTrue("Read packets too slow? (was " + readHeadersTime + ")", readHeadersTime < 10000);
 	}
 }
