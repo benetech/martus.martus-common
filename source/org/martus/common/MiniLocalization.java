724f386a77a1d3561a010e57b7f5aaba92e70939
Rom Srinivasan <Rom Srinivasan@localhost>
2013-03-07 15:46:11 -0800

    moved Zawgyi preference to UILocalization

--- source/org/martus/common/MiniLocalization.java
+++ source/org/martus/common/MiniLocalization.java
@@ -501,18 +501,6 @@ public class MiniLocalization
 	{
 		adjustPersianLegacyDates = newAdjustPersian;
 	}
-
-    public boolean getUseZawgyi()
-    {
-        return useZawgyi;
-    }
-
-    public void setUseZawgyi(boolean newUseZawgyi)
-    {
-        useZawgyi = newUseZawgyi;
-    }
-	
-	
 	
 	private boolean isRightToLeftLanguage()
 	{
@@ -601,6 +589,5 @@ public class MiniLocalization
 	private String currentCalendarSystem;
 	private boolean adjustThaiLegacyDates;
 	private boolean adjustPersianLegacyDates;
-	private boolean useZawgyi;
 
 }
