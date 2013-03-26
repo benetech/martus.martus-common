e1874ab5fd0dedfa1d03af735150f42992b81e4c
Kevin Smith <Kevin Smith@localhost>
2013-03-26 12:04:25 -0400

    Create FieldDeskKey/s (sibling of HeadquarterKey/s)

--- source/org/martus/common/test/TestCommon.java
+++ source/org/martus/common/test/TestCommon.java
@@ -98,6 +98,7 @@ public class TestCommon
 		suite.addTest(new TestSuite(TestDateUtilities.class));
 		suite.addTest(new TestSuite(TestDropDownFieldSpec.class));
 		suite.addTest(new TestSuite(TestFieldDataPacket.class));
+		suite.addTest(new TestSuite(TestFieldDeskKeys.class));
 		suite.addTest(new TestSuite(TestFieldSpec.class));
 		suite.addTest(new TestSuite(TestFileDatabase.class));
 		suite.addTest(new TestSuite(TestFileInputStreamWithSeek.class));
