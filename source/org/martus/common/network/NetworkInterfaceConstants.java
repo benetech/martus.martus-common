1e8d7e792ae9de9ad58975737554aeb0bfd53ced
charlesl <charlesl@localhost>
2014-03-03 14:35:40 -0800

    rename of NOT_FOUND to ITEM_NOT_FOUND

--- source/org/martus/common/network/NetworkInterfaceConstants.java
+++ source/org/martus/common/network/NetworkInterfaceConstants.java
@@ -32,7 +32,7 @@ public interface NetworkInterfaceConstants
 
 	public static final String OK = "ok";
 	public static final String CHUNK_OK = "chunk ok";
-	public static final String NOT_FOUND = "not found";
+	public static final String ITEM_NOT_FOUND = "not found";
 	public static final String REJECTED = "account rejected";
 	public static final String NOTYOURBULLETIN = "not your bulletin";
 	public static final String DUPLICATE = "duplicate";
