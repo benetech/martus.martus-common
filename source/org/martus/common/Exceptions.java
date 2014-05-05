718a420e823c08fa90cf34b2b5f7c0a477f22497
Kevin Smith <Kevin Smith@localhost>
2014-05-05 16:23:42 -0700

    Create ServerNotComptible response and exception

--- source/org/martus/common/Exceptions.java
+++ source/org/martus/common/Exceptions.java
@@ -56,6 +56,10 @@ public class Exceptions
 	{
 	}
 	
+	public static class ServerNotCompatibleException extends Exception
+	{
+	}
+	
 	public static class AccountNotFoundException extends Exception
 	{
 	}
