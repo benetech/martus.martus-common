631b6bf4fb765c818ec34433442e8d326f76bcc4
charlesl <charlesl@localhost>
2015-03-23 08:04:34 -0700

    MARTUSDEV-1346 Replace "bulletins" with "records" in EnglishStrings.java

--- source/org/martus/common/EnglishCommonStrings.java
+++ source/org/martus/common/EnglishCommonStrings.java
@@ -92,7 +92,7 @@ public class EnglishCommonStrings
 		"language:bur=Burmese",
 
 		"field:BulletinUnverifiedFieldDesk=Created by an UNKNOWN Contact",
-		"field:BulletinNotYours=Contact Bulletin",
+		"field:BulletinNotYours=Contact Record",
 		"field:publicsection=Public Information",
 		"field:privatesection=Private Information",
 		"field:allprivate=Keep ALL Information Private",
@@ -117,7 +117,7 @@ public class EnglishCommonStrings
 		"field:attachments=Attachments",
 		"field:HQSummaryLabel=Contacts",
 		"field:HQNotConfigured=(not currently configured)",
-		"field:NoHQsConfigured=No Contact accounts selected for this bulletin.",
+		"field:NoHQsConfigured=No Contact accounts selected for this record.",
 
 		"field:DateRangeTemplatebegin=#FieldLabel# (beginning)",
 		"field:DateRangeTemplateend=#FieldLabel# (end)",
